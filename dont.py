47fb90dafd0e5896277ad338babfe2ae335eb467
Det <detonate@helix.net.nz>
2018-04-10 16:45:24 +0100

    ignore posts that say 'stop'

--- dont.py
+++ dont.py
@@ -7,7 +7,7 @@ import time
 import random
 
 
-key_words = ('disable IPv6', 'disabling IPv6', 'turn off IPv6', 'turning off IPv6')
+key_words = ('disable IPv6', 'disabling IPv6', 'turn off IPv6', 'turning off IPv6', 'stop')
 ignore_words = ('don\'t', 'dont', 'shouldn\'t', '#DontDisableIPv6')
 dont_urls = ('http://techgenix.com/dont-disable-ipv6/',
              'https://biztechmagazine.com/article/2012/03/should-you-disable-ipv6-windows-7-pc',
