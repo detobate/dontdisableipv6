f0b5a779d3491c67d0d9c5d9e238de0c6a409303
Det <detonate@helix.net.nz>
2018-04-27 15:08:24 +0100

    security

--- dont.py
+++ dont.py
@@ -23,6 +23,8 @@ dont_urls = {
                 'https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-8.1-and-8/hh831730(v=ws.11)#EBE'
                 ],
             'faster': ['https://www.howtogeek.com/195062/no-disabling-ipv6-probably-wont-speed-up-your-internet-connection/'],
+            'security': ['https://www.nanog.org/sites/default/files/ebersman2_notr-boston-ipv6-sec.pdf'],
+            'secure': ['https://www.nanog.org/sites/default/files/ebersman2_notr-boston-ipv6-sec.pdf'],
             'generic': [
                 'https://serverfault.com/questions/880537/disadvantage-of-disabling-ipv6',
                 'https://blogs.cisco.com/enterprise/disable-ipv6',
@@ -34,6 +36,8 @@ replies = {
             'windows': ['Windows relies on #IPv6 internally, disabling it will cause issues'],
             'fifa': ['@EA\'s Fifa is notoriously bad at supporting #IPv6 but @XboxSupport still recommends leaving it enabled'],
             'xbox': ['Some Xbox games are poorly written and misbehave on IPv6, but @XboxSupport still recommends leaving it enabled'],
+            'security': ['Instead of ignoring #IPv6, learn about it, embrace it, and secure your network'],
+            'secure': ['Instead of ignoring #IPv6, learn about it, embrace it, and secure your network'],
             'generic': [
                 'Please don\'t disable IPv6, it will break things.',
                 'You shouldn\'t really disable IPv6, it\'s the future of the internet.',
